f820915a28ac2b2efd8a6124c8a8d1bdd001a07f
Alison Fung <90418786+alisonfung@users.noreply.github.com>
2024-11-17 22:26:14 -0800

    add comments

--- crawler.py
+++ crawler.py
@@ -11,25 +11,37 @@ from pymongo import MongoClient
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
 
+# web crawler
 def crawler_thread(frontier, pages, visited):
+    # while there are still pages to visit
     while len(frontier) != 0:
+        # get the first page in the queue
         url = frontier.pop(0)
+        # mark it as visited
         visited.add(url)
         print("visiting", url)
+        # get the html of the page
         html = get_html(url)
+        # if the html was retrieved:
         if html:
+            # store the page data in the database
             store_page(pages, url, html, False)
+            # if the page is the target:
             if check_target(html):
+                # update the database record
                 store_page(pages, url, html, True)
+                # clear frontier to stop the web crawler
                 frontier = []
+            # if the page wasn't the target:
             else:
+                # get all links on the page
                 links = get_links(html)
+                # add unvisited links to the frontier
                 for link in links:
                     if link not in visited:
                         frontier.append(link)
-                #print("need to visit", frontier)
 
-
+# get the html from a url
 def get_html(url):
     try:
         html = urlopen(url)
@@ -38,41 +50,51 @@ def get_html(url):
     except URLError as e:
         print(url, "could not be found", e)
     else:
+        # if html successfully retrieved, return it
         return html.read().decode('utf-8')
+    # if any errors happened, return nothing
     return ""
 
+# check if page is the target page
 def check_target(html):
     bs = BeautifulSoup(html, "html.parser")
     return bs.find("h1", {"class": "cpp-h1"}, string="Permanent Faculty")
 
+# get all links from a page
 def get_links(html):
     bs = BeautifulSoup(html, "html.parser")
+    # find all anchor tags and grab their links
     links = [tag["href"] for tag in bs.find_all("a")]
     for i in range (len(links)-1):
+        # if this is a relative link, add the base link
         if links[i][0] == "/":
             links[i] = "https://www.cpp.edu" + links[i]
-    for link in links:
-        links = [link for link in links if "https://www.cpp.edu/" in link]
+    # remove all links that are not from cpp
+    links = [link for link in links if "https://www.cpp.edu/" in link]
     return links
 
+# store a page in the database
 def store_page(col, url, html, target):
-    # store page
     document = { "url": url,
                  "html": html,
                  "target": target
     }
-    col.update_one({"_id": url}, {"$set": {"_id": url, "html": html, "target": target}}, True)
+    # use upsert to handle both new entries and updates
+    col.update_one({"_id": url}, {"$set": {"url": url, "html": html, "target": target}}, True)
 
+# connect to the database
 def connect_database():
     client = MongoClient(host="localhost", port=27017)
     db = client.assignment3
     return db
 
 def main():
+    # connect to database and set up frontier url plus visited set
     db = connect_database()
     pages = db["pages"]
     visited = set()
     frontier = ["https://www.cpp.edu/sci/computer-science/"]
+    # call crawler
     crawler_thread(frontier, pages, visited)
 
 if __name__  == "__main__":
--- parser.py
+++ parser.py
@@ -10,6 +10,7 @@ import re
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
+# connect to database
 def connect_database():
     client = MongoClient(host="localhost", port=27017)
     db = client.assignment3
@@ -17,47 +18,55 @@ def connect_database():
 
 def main():
     db = connect_database()
+    # set up database
     professors = db["professors"]
     pages = db["pages"]
     # retrieve html document
     html = pages.find_one({"target": True}).get("html")
-    #name, title, office, phone, email, and
     bs = BeautifulSoup(html, "html.parser")
+    # get all divs in clearfix class for professor information
     data = bs.find_all("div", {"class": "clearfix"})
     for div in data:
+        # get all professor names in h2 tags
         prof_names = div.find_all("h2")
         for name in prof_names:
             prof_info = {}
+            # access the p tag
             info = name.next_sibling
-            prof_info["name"] = name.get_text().replace("\xa0", "")
+            # get name, fix spacing
+            prof_info["name"] = name.get_text().replace("\xa0", "").strip()
+            # get title after "Title" strong tag, fix spacing
             prof_info["title"] = (info.next_sibling.find('strong', string=re.compile("Title"))
                                   .next_sibling.get_text().replace("\xa0", " ")
                                   .translate(str.maketrans('', '', ":"))
                                   .strip())
+            # get office after "Office" strong tag, fix spacing
             prof_info["office"] = (info.next_sibling.find('strong', string=re.compile("Office"))
                                   .next_sibling.get_text().replace("\xa0", " ")
                                   .translate(str.maketrans('', '', ":"))
                                   .strip())
+            # get phone after "Phone" strong tag, fix spacing
             prof_info["phone"] = (info.next_sibling.find('strong', string=re.compile("Phone"))
                                   .next_sibling.get_text().replace("\xa0", " ")
                                   .translate(str.maketrans('', '', ":"))
                                   .strip())
+            # get email from anchor tag, after "Website" strong tag, fix spacing
             prof_info["email"] = (info.next_sibling.find('strong', string=re.compile("Email"))
                                  .next_sibling.next_sibling.get_text().replace("\xa0", " ")
                                  .translate(str.maketrans('', '', ":"))
                                  .strip())
+            # get website from anchor tag, after "Web" strong tag, fix spacing
             try:
                 prof_info["website"] = (info.next_sibling.find('strong', string=re.compile("Web"))
                                    .next_sibling.next_sibling.get_text().replace("\xa0", " ")
                                    .translate(str.maketrans('', '', ":"))
                                    .strip())
+            # catch exception for no websites
             except:
                 prof_info["website"] = "No website"
             print(prof_info)
+            # insert professor into the collection
             professors.insert_one(prof_info)
-
-
-
 
 if __name__  == "__main__":
     main()
